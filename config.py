ff4eefe7e95a66f05f344d2a88dfa8f6c222645d
MrCripe <60510751+MrCripe@users.noreply.github.com>
2022-01-02 02:49:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ command_prefixes = ['.','!','/']
 
 ghoul_table_command = 'ghoul-c'
 
-end_message = '
-Кто прочитал тот гей' # Сообщение после конца цикла, если не нужно - оставляем пустым
+end_message = 'Кто прочитал тот гей' # Сообщение после конца цикла, если не нужно - оставляем пустым
 messages_per_second = 5 # Для ghoul_spam
 sleep_time_ghoul = 0.1 # Для ghoul_table 0.0825
 
